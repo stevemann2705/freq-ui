cc5f530a53f205f88701378fe2f0f2fef1a5761b
stevemann <steve.mann@technifybiz.com>
2021-06-05 22:25:51 +0530

    added last trade time on homepage

---
+++ src/layouts/home/lastTradeTime.js
@@ -0,0 +1,20 @@
+import {Header, Segment} from "semantic-ui-react";
+import React from "react";
+
+const square = {width: 200, height: 200}
+
+const LastTradeTimeUI = (props) => {
+    console.log("props", props);
+    return (
+        <div style={{paddingTop: '10px', paddingLeft: '30px', paddingRight: '30px'}}>
+            <Segment circular style={square}>
+                <Header as='h2'>
+                    Last Trade
+                    <Header.Subheader>{props?.state}</Header.Subheader>
+                </Header>
+            </Segment>
+        </div>
+    );
+};
+
+export default LastTradeTimeUI;
