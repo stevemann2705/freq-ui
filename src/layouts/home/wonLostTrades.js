2ea233e76d1c123b34d795ad29ee318997fc6810
stevemann <steve.mann@technifybiz.com>
2021-06-05 21:27:20 +0530

    added won/lost trades on homepage

--- src/layouts/home/wonLostTrades.js
+++ src/layouts/home/wonLostTrades.js
@@ -1,20 +1,19 @@
 import {Header, Segment} from "semantic-ui-react";
 import React from "react";
-import {quantityFormat} from "../../utils/quantityFormat";
 
 const square = {width: 200, height: 200}
 
-const CurrentBalanceUI = (props) => {
+const WonLostUI = (props) => {
     return (
         <div style={{paddingTop: '10px', paddingLeft: '30px', paddingRight: '30px'}}>
             <Segment circular style={square}>
                 <Header as='h2'>
-                    Total Profit
-                    <Header.Subheader>{"$" + quantityFormat(props?.state)}</Header.Subheader>
+                    Won/Lost
+                    <Header.Subheader>{props?.state}</Header.Subheader>
                 </Header>
             </Segment>
         </div>
     );
 };
 
-export default CurrentBalanceUI;
+export default WonLostUI;
