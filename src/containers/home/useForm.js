dd5c34b2a89d56bf4b8b593fd1441d7b1b549e3a
stevemann <steve.mann@technifybiz.com>
2021-06-05 20:58:00 +0530

    added total profit on homepage

--- src/containers/home/useForm.js
+++
@@ -1,9 +0,0 @@
-import api from "../../helpers/api";
-
-const getBalance = () => {
-     return api.balance();
-    // console.log("balance", balance);
-    // return {balance};
-};
-
-export default getBalance;
